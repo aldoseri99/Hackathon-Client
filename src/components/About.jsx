f770bd99869e56b6c05f8e886baecef99a32276f
Zahralsayed <zalmatooq31@gmail.com>
2024-10-14 21:36:44 +0300

    Add About Page

---
+++ src/components/About.jsx
@@ -0,0 +1,16 @@
+const About = () => {
+  return (
+    <div>
+      <h2>About</h2>
+      <h4>Welcome to the RideRadar App,</h4>
+      <p>
+        your ultimate guide to the thrilling world of rollercoasters! Whether
+        you are a thrill-seeker or just curious about the exciting rides at your
+        favorite theme parks, our app is designed to provide you with all the
+        essential information you need.
+      </p>
+    </div>
+  )
+}
+
+export default About
