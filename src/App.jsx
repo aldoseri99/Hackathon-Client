0ef25f67c0f05436ff6c3daefc9ccb416b50d69e
Zahralsayed <zalmatooq31@gmail.com>
2024-10-14 23:46:09 +0300

    nav bar done, adding some style

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
+import { Route, Routes } from "react-router-dom"
+import Nav from "./components/Nav"
+import About from "./components/About"
+import "./App.css"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div></div>
-    </>
+    <div>
+      <Nav />
+      <main>
+        <Routes>
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </main>
+    </div>
   )
 }
 
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,13 +1,12 @@
 const About = () => {
   return (
-    <div>
+    <div className="about">
       <h2>About</h2>
-      <h4>Welcome to the RideRadar App,</h4>
       <p>
-        your ultimate guide to the thrilling world of rollercoasters! Whether
-        you are a thrill-seeker or just curious about the exciting rides at your
-        favorite theme parks, our app is designed to provide you with all the
-        essential information you need.
+        Welcome to the RideRadar App, your ultimate guide to the thrilling world
+        of rollercoasters! Whether you are a thrill-seeker or just curious about
+        the exciting rides at your favorite theme parks, our app is designed to
+        provide you with all the essential information you need.
       </p>
     </div>
   )
