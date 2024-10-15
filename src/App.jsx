841a085c5aa5e632f93943eb36950c535e1f4788
aldoseri99 <aldosariali9@gmail.com>
2024-10-15 16:04:55 +0300

    Register Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,42 @@
-import { useState } from "react"
-import { Route, Routes } from "react-router-dom"
-import Nav from "./components/Nav"
-import About from "./components/About"
-import Home from "./pages/Home"
-import "./App.css"
-
+import { useState, useEffect } from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Nav from './components/Nav'
+import About from './components/About'
+import Home from './pages/Home'
+import './App.css'
+import Register from './pages/Register'
+import SignIn from './pages/Signin'
+import { CheckSession } from './services/Auth'
 function App() {
-  const [count, setCount] = useState(0)
+  const [user, setUser] = useState(null)
 
+  const handleLogOut = () => {
+    //Reset all auth related state and clear localStorage
+    setUser(null)
+    localStorage.clear()
+  }
+
+  const checkToken = async () => {
+    const user = await CheckSession()
+    setUser(user)
+  }
+
+  useEffect(() => {
+    const token = localStorage.getItem('token')
+
+    if (token) {
+      checkToken()
+    }
+  }, [])
   return (
     <div>
       <Nav />
       <main>
         <Routes>
-          <Route path="/about" element={<About />} />
           <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/signin" element={<SignIn setUser={setUser} />} />
         </Routes>
       </main>
     </div>
