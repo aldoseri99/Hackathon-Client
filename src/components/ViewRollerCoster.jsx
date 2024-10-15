2d679c9c91046f17613132d86cf6d373d851f5a1
sayed115 <sayedmohamed11577@gmail.com>
2024-10-15 23:42:46 +0300

    css

--- src/components/ViewRollerCoster.jsx
+++ src/components/ViewRollerCoster.jsx
@@ -17,21 +17,21 @@ const ViewRollerCoser = () => {
   }
 
   return (
-    <div>
+    <div className="main-grid">
       {costers.map((coster) => (
         <div className="card1" onClick={() => selectCoster(coster.id)} key={coster.id}>
           <div className="img">
-            <img src={`http://localhost:3001/uploads/${coster.image}`} alt="coster Image" />
+            <img className="img-card" src={`http://localhost:3001/uploads/${coster.image}`} alt="coster Image" />
           </div>
 
           <div className="costerInfo">
             <h3>{coster.name}</h3>
-            <p>{coster.location}</p>
+            <p className="cll">{coster.location}</p>
+            <h3 className="rating">{coster.rating}</h3>
+          
           </div>
 
-          <div className="cardRating">
-            <h3>{coster.rating}</h3>
-          </div>
+          
         </div>
       ))}
     </div>
