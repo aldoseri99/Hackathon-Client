cea27261ad1e439a302962e04a6b6158cdb5382c
Zahralsayed <zalmatooq31@gmail.com>
2024-10-15 15:35:45 +0300

    Add page done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { Route, Routes } from "react-router-dom"
 import Nav from "./components/Nav"
 import About from "./components/About"
+import Add from "./components/Add"
+import axios from "axios"
 import "./App.css"
 
 function App() {
+  const [rollerCoaster, setRollerCoaster] = useState([])
+
+  /* const getRollerCoaster = async () => {
+    try {
+      let res = await axios.get("http://localhost:3001/rollerCoaster")
+      setRollerCoaster(res.data)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  useEffect(() => {
+    getRollerCoaster()
+  }, [])*/
+
   const [count, setCount] = useState(0)
 
   return (
@@ -14,6 +31,10 @@ function App() {
         <Routes>
           <Route path="/about" element={<About />} />
         </Routes>
+        <Add
+          rollerCoaster={rollerCoaster}
+          setRollerCoaster={setRollerCoaster}
+        />
       </main>
     </div>
   )
